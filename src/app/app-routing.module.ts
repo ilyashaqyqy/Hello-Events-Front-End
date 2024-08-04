d9dc791991b49b6f5577742fba99de4083077d3c
ilyashq <ilyasenaa24@gmail.com>
2024-08-04 17:51:11 +0100

    Ajout de gardes d'authentification et d'autorisation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './client/auth/register/register.component';
 import { HomeComponent } from './client/home/home.component';
 import { LoginComponent } from './client/auth/login/login.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AuthGuard } from './gaurds/auth.guard';
+import { AdminGuard } from './gaurds/admin.guard';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component:HomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent , canActivate: [AuthGuard, AdminGuard]},
 
   { path: '', redirectTo: '/register', pathMatch: 'full' }
   // { path: '', redirectTo: '/login', pathMatch: 'full' }
